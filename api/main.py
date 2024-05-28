349b18e1b6fc0aee7ef45b610afc41615fe1d3c7
yunusemreerkesikbas <20060364@stu.omu.edu.tr>
2024-05-28 23:19:13 +0300

    OMU-002 api connection

--- api/main.py
+++ api/main.py
@@ -1,13 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+import logging
+import pickle
 
+from models.seq2seq_model import Seq2SeqModel
+
+# Logging yapılandırması
+logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
+# CORS middleware ekleme
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+seq2seq = Seq2SeqModel()
+model_path = os.path.join('/home/enoca2/Desktop/OMUChatbot/models/seq2seq_model.keras')
+seq2seq.load_model(model_path)
 
+class RequestBody(BaseModel):
+    question: str
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/predict/")
+async def predict(request_body: RequestBody):
+    question = request_body.question
+    logging.info(f"Question: {question}")
+    response = seq2seq.generate_response(question)
+    logging.info(f"Response: {response}")
+    return {"response": response}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
